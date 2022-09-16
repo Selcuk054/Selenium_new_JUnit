98b237150312779719098ff1b2f5d91cfca74ddb
Selcuk Ufuk <selcukkrds09@gmail.com>
2022-09-16 22:16:33 +0200

    16.09

--- src/test/java/day06/Odev_YoutubeAssertion.java
+++ src/test/java/day06/Odev_YoutubeAssertion.java
@@ -1,6 +1,16 @@
 package day06;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class Odev_YoutubeAssertion {
  /*   1) Bir class oluşturun: YoutubeAssertions
@@ -11,8 +21,40 @@ public class Odev_YoutubeAssertion {
          ○ Search Box 'in erisilebilir oldugunu test edin (isEnabled())
          ○ wrongTitleTest => Sayfa basliginin “youtube” olmadigini dogrulayin
   */
+    WebDriver driver;
     @Before
-    public static void setUp(){
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver =new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        //2) https://www.youtube.com adresine gidin
+        driver.get("https://www.youtube.com");
+    }
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+    @Test
+    public void titleTest(){
+        //Sayfa başlığının “YouTube” oldugunu test edin
+        //Varsa cokies reddet
+        driver.findElement(By.xpath("(//*[@class='style-scope ytd-button-renderer style-primary size-default'])[1]")).click();
+        String expectedTitle = "YouTube";
+        String actuelTitle = driver.getTitle();
 
-    } 
+        Assert.assertTrue(actuelTitle.equals(expectedTitle));
+    }
+    @Test
+    public void imageTest(){
+        //○ imageTest => YouTube resminin görüntülendiğini (isDisplayed()) test edin
+       WebElement image = driver.findElement(By.xpath("(//*[@class='style-scope ytd-logo'])[1]"));
+       Assert.assertTrue(image.isDisplayed());
+    }
+    @Test
+    public void wrongTitleTest(){
+        // ○ wrongTitleTest => Sayfa basliginin “youtube” olmadigini dogrulayin
+        String expectedTitle = "youtube";
+        String actuelTitle = driver.getTitle();
+        Assert.assertFalse(expectedTitle.equals(actuelTitle));
+    }
 }
