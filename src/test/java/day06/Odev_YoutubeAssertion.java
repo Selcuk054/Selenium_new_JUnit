c84762d935d2af047bfb948b2b70bd3ed10d2fd3
Selcuk Ufuk <selcukkrds09@gmail.com>
2022-09-16 19:35:41 +0200

    16.09

---
+++ src/test/java/day06/Odev_YoutubeAssertion.java
@@ -0,0 +1,18 @@
+package day06;
+
+import org.junit.Before;
+
+public class Odev_YoutubeAssertion {
+ /*   1) Bir class oluşturun: YoutubeAssertions
+      2) https://www.youtube.com adresine gidin
+      3) Aşağıdaki adları kullanarak 3 test metodu oluşturun ve gerekli testleri yapin
+         ○ titleTest => Sayfa başlığının “YouTube” oldugunu test edin
+         ○ imageTest => YouTube resminin görüntülendiğini (isDisplayed()) test edin
+         ○ Search Box 'in erisilebilir oldugunu test edin (isEnabled())
+         ○ wrongTitleTest => Sayfa basliginin “youtube” olmadigini dogrulayin
+  */
+    @Before
+    public static void setUp(){
+
+    } 
+}
