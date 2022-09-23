de55f40e5f3541842b8b42ecdf975a96347b2aad
Selcuk Ufuk <selcukkrds09@gmail.com>
2022-09-23 20:36:05 +0200

    23.09

---
+++ src/test/java/day13/C01_Cokies.java
@@ -0,0 +1,54 @@
+package day13;
+
+
+import org.junit.Assert;
+import org.openqa.selenium.Cookie;
+import org.junit.Test;
+import utilities.TestBaseBeforeAfter;
+
+import java.util.Set;
+
+public class C01_Cokies extends TestBaseBeforeAfter {
+    @Test
+    public void test1() {
+        // 1-Amazon anasayfaya gidin
+        driver.get("https://amazon.com");
+        //2-tum cookie’leri listeleyin
+        Set<Cookie> tumCookie = driver.manage().getCookies();
+        System.out.println(tumCookie);
+        int sayac = 1;
+        for (Cookie w : tumCookie) {
+            System.out.println(sayac + ".ci cookie :" + w);
+            System.out.println(sayac + ".ci name :" + w.getName());
+            System.out.println(sayac + ".ci value :" + w.getValue());
+            sayac += 1;
+        }
+        //3-Sayfadaki cookies sayisinin 5’den buyuk oldugunu test edin
+        int cookieSayisi = tumCookie.size();
+        Assert.assertTrue(cookieSayisi > 5);
+        //4-ismi i18n-prefs olan cookie degerinin USD oldugunu test edin.
+        for (Cookie w : tumCookie) {
+            if (w.getName().equals("i18n-prefs")) {
+                Assert.assertEquals("USD", w.getValue());
+            }
+        }
+        // 5-ismi “en sevdigim cookie” ve degeri “cikolatali” olan bir cookie  olusturun ve sayfaya ekleyin
+        Cookie yeniCookie = new Cookie("en sevdigim cookie", "cikolatali");
+        driver.manage().addCookie(yeniCookie);
+        tumCookie = driver.manage().getCookies();
+        int sayac1 = 1;
+        for (Cookie w : tumCookie) {
+            System.out.println(sayac1 + ".ci cookie :" + w);
+            System.out.println(sayac1 + ".ci name :" + w.getName());
+            System.out.println(sayac1 + ".ci value :" + w.getValue());
+            sayac1 += 1;
+            //6-eklediginiz cookie’nin sayfaya eklendigini test edin
+            //7-ismi skin olan cookie’yi silin ve silindigini test edin
+            //8-tum cookie’leri silin ve silindigini test edin
+
+
+        }
+
+
+    }
+}
