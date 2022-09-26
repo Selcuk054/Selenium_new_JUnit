8c2ab99fb026098b3b3ddb6ce85abe9fbee62300
Selcuk Ufuk <selcukkrds09@gmail.com>
2022-09-26 19:40:45 +0200

    26.09

---
+++ src/test/java/day13/C02_WebTables.java
@@ -0,0 +1,11 @@
+package day13;
+
+import org.junit.Test;
+import utilities.TestBaseBeforeAfter;
+
+public class C02_WebTables extends TestBaseBeforeAfter {
+    @Test
+    public void test1(){
+        
+    }
+}
